1d5215f71b83285b0dd750b888914929fc4a1c28
OUSmartInfrastructure <33726869+OUSmartInfrastructure@users.noreply.github.com>
2018-05-09 11:18:04 -0400

    Add files via upload

---
+++ src/constants.h
@@ -0,0 +1,6 @@
+#define MQTT_TOPIC "ITS5900/iot_prj02_sensor"
+
+// Debug levels on (1) or off (0)
+#define DBGLVL0         1 // Human action info
+#define DBGLVL1         1 // System action info
+#define DBGLVL2         1 // State changne info
