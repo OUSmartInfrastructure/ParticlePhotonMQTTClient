f674b24122db2b165978c2b6b2da709c7bf20750
OUSmartInfrastructure <33726869+OUSmartInfrastructure@users.noreply.github.com>
2018-05-09 11:15:43 -0400

    Add files via upload

---
+++ cert/cert.h
@@ -0,0 +1,15 @@
+//https://docs.aws.amazon.com/iot/latest/developerguide/managing-device-certs.html
+#define AMAZON_IOT_ROOT_CA_PEM                                          \
+"-----BEGIN CERTIFICATE----- \r\n"                                      \
+"[ROOT CA CRT PEM]                                       \r\n"  \
+"-----END CERTIFICATE----- "
+
+#define CELINT_KEY_CRT_PEM                                              \
+"-----BEGIN CERTIFICATE----- \r\n"                                      \
+"[YOUR CLIENT KEY CRT PEM]                                       \r\n"  \
+"-----END CERTIFICATE----- "
+
+#define CELINT_KEY_PEM                                                  \
+"-----BEGIN RSA PRIVATE KEY-----\r\n"                                   \
+"[YOUR PRIVATE KEY PEM]                                          \r\n"  \
+"-----END RSA PRIVATE KEY----- "
